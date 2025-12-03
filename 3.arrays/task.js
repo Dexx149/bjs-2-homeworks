e77c075682b80103215c4c1a4efbd6e003f391e8
Dexx149 <91342462+Dexx149@users.noreply.github.com>
2025-12-03 14:36:16 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
   
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  
+  return true;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filtered = users.filter(user => user.gender === gender);
+  return filtered.length ? filtered.reduce((sum, user) => sum + user.age, 0) / filtered.length : 0;
 }
