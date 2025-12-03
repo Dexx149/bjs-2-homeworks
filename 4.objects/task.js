144f4362a96502f230ee00c4b4d9c5313b3882ea
Dexx149 <91342462+Dexx149@users.noreply.github.com>
2025-12-03 14:41:59 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,36 +1,36 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.age = age;
-  this.gender = gender;
-  this.marks = [];
+	this.name = name;
+	this.age = age;
+	this.gender = gender;
+	this.marks = [];
 }
 
 Student.prototype.setSubject = function(subjectName) {
-  this.subject = subjectName;
+	this.subject = subjectName;
 };
 
 Student.prototype.addMarks = function(...marksToAdd) {
-  if (this.excluded) {
+	if (this.excluded) {
 
-    console.log(`Студент ${this.name} отчислен, нельзя добавить оценки`);
-    return;
-  }
-  this.marks.push(...marksToAdd);
+		console.log(`Студент ${this.name} отчислен, нельзя добавить оценки`);
+		return;
+	}
+	this.marks.push(...marksToAdd);
 };
 
 Student.prototype.getAverage = function() {
-  if (!this.marks || this.marks.length === 0) {
-    return 0;
-  }
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
 
-  const sum = this.marks.reduce((total, mark) => total + mark, 0);
-  return sum / this.marks.length;
+	const sum = this.marks.reduce((total, mark) => total + mark, 0);
+	return sum / this.marks.length;
 };
 
 Student.prototype.exclude = function(reason) {
 
-  delete this.subject;
-  delete this.marks;
-  
-  this.excluded = reason;
+	delete this.subject;
+	delete this.marks;
+
+	this.excluded = reason;
 };
