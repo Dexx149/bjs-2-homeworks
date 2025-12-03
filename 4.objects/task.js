cd419e3021a5f4959c5c932e7897a9acceaeb864
Dexx149 <91342462+Dexx149@users.noreply.github.com>
2025-12-03 14:39:18 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+  if (this.excluded) {
 
-Student.prototype.getAverage = function () {
-  
-}
+    console.log(`Студент ${this.name} отчислен, нельзя добавить оценки`);
+    return;
+  }
+  this.marks.push(...marksToAdd);
+};
 
-Student.prototype.exclude = function (reason) {
+Student.prototype.getAverage = function() {
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+
+  const sum = this.marks.reduce((total, mark) => total + mark, 0);
+  return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function(reason) {
+
+  delete this.subject;
+  delete this.marks;
   
-}
+  this.excluded = reason;
+};
