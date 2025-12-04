13f015559a4a4d95b0b80cc8036c167c3ff43746
Dexx149 <91342462+Dexx149@users.noreply.github.com>
2025-12-04 13:39:28 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,62 @@
-﻿
+function parseCount(value) {
+	const parsedValue = Number.parseFloat(value);
+
+	if (Number.isNaN(parsedValue)) {
+		throw new Error("Невалидное значение");
+	}
+
+	return parsedValue;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(side1, side2, side3) {
+		if (side1 + side2 <= side3 ||
+			side1 + side3 <= side2 ||
+			side2 + side3 <= side1) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+
+	get perimeter() {
+		return this.side1 + this.side2 + this.side3;
+	}
+
+	get area() {
+		const semiPerimeter = this.perimeter / 2;
+		const area = Math.sqrt(
+			semiPerimeter *
+			(semiPerimeter - this.side1) *
+			(semiPerimeter - this.side2) *
+			(semiPerimeter - this.side3)
+		);
+
+		return parseFloat(area.toFixed(3));
+	}
+}
+
+function getTriangle(side1, side2, side3) {
+	try {
+		return new Triangle(side1, side2, side3);
+	} catch (error) {
+		return {
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			}
+		};
+	}
+}﻿
